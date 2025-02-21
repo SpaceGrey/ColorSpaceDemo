0539a851a7d1e1fd2d39e079556466fea817443d
SpaceGrey <78807862+SpaceGrey@users.noreply.github.com>
2025-02-21 08:30:00 +0800

    Initial Commit

---
+++ ColorSpaceDemo/ColorSpaceDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorSpaceDemoApp.swift
+//  ColorSpaceDemo
+//
+//  Created by 王培屹 on 21/2/25.
+//
+
+import SwiftUI
+
+@main
+struct ColorSpaceDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ColorSpaceDemo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ColorSpaceDemo
+//
+//  Created by 王培屹 on 21/2/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
